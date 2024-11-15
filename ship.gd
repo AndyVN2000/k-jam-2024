0e6e769f0af6558b6d862a997b5a5ae39206f4a5
andy <202007621@post.au.dk>
2024-11-15 18:09:52 +0100

    Ship can rotate

---
+++ ship.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var speed = 400
+var angular_speed = PI
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	
+	rotation += angular_speed * direction * delta
