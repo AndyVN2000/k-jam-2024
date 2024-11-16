67eea8639034c32d75ea637aa6f6cbba1b3df095
andy <andyvu@outlook.dk>
2024-11-16 19:25:05 +0100

    Both cannon ball pickup and drop graphics works

--- cannon_ball_container.gd
+++ cannon_ball_container.gd
@@ -5,7 +5,14 @@ var cannon_ball_image: TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Listen to signals when player picks up or drops an item
 	player.cannon_ball_pickup.connect(_on_cannon_ball_pickup)
+	player.cannon_ball_drop.connect(_on_cannon_ball_drop)
+	
+	# Prepare the item images
+	cannon_ball_image = TextureRect.new()
+	var image = preload("res://gui/cannon_ball_v1.png")
+	cannon_ball_image.texture = image
 	pass # Replace with function body.
 
 
@@ -14,7 +21,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_cannon_ball_pickup():
-	cannon_ball_image = TextureRect.new()
-	var image = preload("res://gui/cannon_ball_v1.png")
-	cannon_ball_image.texture = image
 	add_child(cannon_ball_image)
+
+func _on_cannon_ball_drop():
+	remove_child(cannon_ball_image)
