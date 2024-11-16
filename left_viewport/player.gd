ac563983ac45b5ebb32d19d56c6607bc4d59dfd5
andy <andyvu@outlook.dk>
2024-11-16 13:38:07 +0100

    Organized folder structure

--- left_viewport/player.gd
+++ left_viewport/player.gd
@@ -1,21 +1,19 @@
-extends CharacterBody2D
+extends Area2D
 
+var speed = 150
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 
-
-func _physics_process(delta: float) -> void:
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
