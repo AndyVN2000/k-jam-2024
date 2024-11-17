c254fa3c6d01e1660e652f29f525bf39926b15d4
andy <andyvu@outlook.dk>
2024-11-17 01:52:05 +0100

    Polishing animation and controls

--- right_viewport/ship.gd
+++ right_viewport/ship.gd
@@ -17,9 +17,9 @@ func _process(delta: float) -> void:
 		return
 	
 	var direction = 0
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		direction = -1
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		direction = 1
 	
 	rotation += angular_speed * direction * delta
